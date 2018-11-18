4681658f98bed13d0df52502849dd288f9d4eac5
Elana Kopelevich <ekopelevich@gmail.com>
2018-11-18 16:52:02 -0700

    display date

--- src/components/til_card/index.js
+++ src/components/til_card/index.js
@@ -2,10 +2,14 @@ import React from 'react'
 import './index.scss'
 
 const TilCard = props => {
+  console.log(props.date)
   return (
     <div className="card">
-      <p>{props.content}</p>
-      <div className="tags">{props.tags.map(tag => <span className="tag" key={tag}>#{tag}</span>)}</div>
+      <div className="card-header">{props.date.toString()}</div>
+      <div className="card-body">
+        <p>{props.content}</p>
+        <div className="tags">{props.tags.map(tag => <span className="tag" key={tag}>#{tag}</span>)}</div>
+      </div>
     </div>
   )
 }
