7cc42fd8bfa55b4af523be00fdc05ddff42cfb72
Elana Kopelevich <ekopelevich@gmail.com>
2019-01-05 15:34:46 -0700

    Style 404 page and add react-kawaii (for ghost)

--- src/containers/no_match_container/index.js
+++ src/containers/no_match_container/index.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import { Ghost } from 'react-kawaii'
 import './index.scss'
 
 class NoMatchContainer extends Component {
   render() {
     return (
-      <div>404</div>
+      <div className="wrapper">
+        <h1>404</h1>
+        <h4>It looks like the page you are looking for doesn't exist.</h4>
+        <Ghost size={240} mood="sad" color="#4682B3" />
+      </div>
     )
   }
 }
