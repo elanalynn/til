0bb2133b02481cbb8b04bf47f03d729fced5b646
Elana Kopelevich <ekopelevich@gmail.com>
2019-01-05 15:54:20 -0700

    add signup and signin links to the header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 import './index.scss'
 
 const Header = props => {
@@ -6,10 +7,16 @@ const Header = props => {
     <header className="header">
       <div className="wrapper">
         <h1>til</h1>
-        {props.user.id && <div className="items-right">
+        { props.user.id ? <div className="items-right">
           <p>Hi {props.user.first_name}!</p>
           <a href="/" onClick={props.logout}>Logout</a>
-        </div>}
+        </div>
+        : <div className="items-right">
+          <Link to="/signup">Sign Up</Link>
+          <span> / </span>
+          <Link to="/signin">Sign In</Link>
+        </div> 
+        }
       </div>
     </header>
   )
