47765b34d8cd4a824d700fc01e42abe6628c2ae9
Elana Kopelevich <ekopelevich@gmail.com>
2019-01-06 12:29:21 -0700

    hardcode -other learners- and scss

--- src/containers/index_container/index.js
+++ src/containers/index_container/index.js
@@ -1,21 +1,40 @@
 import React, { Component } from 'react'
-import { Redirect } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { Cat } from 'react-kawaii'
+import LearnerCard from './learner_card'
 import './index.scss'
 
 class IndexContainer extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      learners: [
+        { first_name: 'Abby', tags: ['kubernetes', 'docker'] },
+        { first_name: 'John', tags: ['css', 'design systems'] },
+        { first_name: 'Igor', tags: ['racecar driving', 'history'] }]
+    }
+  }
+
   render() {
     return (
-      <div>
-        { this.props.user
-          ? <Redirect to={`/${this.props.user.id}/items`} />
-        : <div className="wrapper index-container">
-          <section>
-            <h1>Today I Learned...</h1>
-            <h4>Take note of things you learn.</h4>
-          </section>
-          <Cat size={360} mood="excited" color="#4682B3" />
-        </div> }
+      <div className="wrapper index-container">
+        <section>
+          <h1>Today I Lerned...</h1>
+          <h4>Take note of things you learn.</h4>
+          { this.props.user && <div>
+            <hr />
+            <Link to={`/${this.props.user.id}/items`} className="button primary">
+              <h2>Go to your Dashboard...</h2>
+            </Link>
+          </div> }
+          <hr />
+          <h2>See what others are learning!</h2>
+          <div className="other-learners">
+            {this.state.learners.map(l => <LearnerCard learner={l} key={`l-${l.first_name}`}/>)}
+          </div>
+        </section>
+        <Cat size={360} mood="excited" color="#4682B3" />
       </div>
     )
   }
---
+++ src/containers/index_container/learner_card/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './index.scss'
+
+const LearnerCard = props => {
+  console.log(props.learner)
+  return (
+    <div className="learner-card">
+      <div className="card-body">
+        <p>{props.learner.first_name}</p>
+        {props.learner.tags.map(tag => <span className="tag" key={`tag-${tag}`}>#{tag}</span>)}
+      </div>
+    </div>
+  )
+}
+
+export default LearnerCard
